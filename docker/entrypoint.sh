3cce5a71c2155f10b07ae521c8c566654c849ef6
Michael Davison <michael.davison@gmail.com>
2024-06-05 13:19:27 -0600

    reorder

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,8 +8,8 @@ mkdir -p /home/rstudio/.local/share/tidyhydat
 cp /work/Hydat.sqlite3 /home/rstudio/.local/share/tidyhydat/
 chown -R rstudio:rstudio /home/rstudio/.local/share/tidyhydat
 
-sudo -u rstudio /home/rstudio/workspace/BRBC_SOW/vscode.sh # install extensions
 export DONT_PROMPT_WSL_INSTALL=1
 sudo -u rstudio code serve-web --port 28080 --accept-server-license-terms --host 0.0.0.0 --without-connection-token 2>&1 | tee -a /tmp/code-server.log &
+sudo -u rstudio /home/rstudio/workspace/BRBC_SOW/vscode.sh # install extensions
 
 /usr/lib/rstudio-server/bin/rserver --server-daemonize 0 --auth-none 1
