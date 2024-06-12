3fb232d8bbd9fa9bbfc10f58302886785e6a7a7b
Michael Davison <michael.davison@gmail.com>
2024-06-12 12:41:29 -0600

    this can't really be done before the remote has been created :/

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -10,6 +10,5 @@ chown -R rstudio:rstudio /home/rstudio/.local/share/tidyhydat
 
 export DONT_PROMPT_WSL_INSTALL=1
 sudo -u rstudio code serve-web --port 28080 --accept-server-license-terms --host 0.0.0.0 --without-connection-token 2>&1 | tee -a /tmp/code-server.log &
-sudo -u rstudio /home/rstudio/workspace/BRBC_SOW/vscode.sh # install extensions
 
 /usr/lib/rstudio-server/bin/rserver --server-daemonize 0 --auth-none 1
